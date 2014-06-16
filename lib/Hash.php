fd8997696fa5d26462ddc06a22add4e44c0021a3
Hieu Tran <trunghieu.tran138@gmail.com>
2014-06-16 20:09:43 +0300

    Comment

--- lib/Hash.php
+++ lib/Hash.php
@@ -3,12 +3,11 @@
 class Hash {
 		
 	/**
-	  * @param string $algo The alogrithm(md5,sha1,...)
-	  * @param string $data The data to encode
-	  * @param string $salt (Should be remain the same in the system)
-	  * @return string The hashed/salted data
-	 */
-		
+	  * @param string $algo The alogrithm (eg. md5, sha1).
+	  * @param string $data The data to be hashed.
+	  * @param string $salt (Should be remain the same in the system).
+	  * @return string The hashed/salted data.
+	*/		
 	public static function create($algo, $data, $salt) {
 		$context = hash_init($algo, HASH_HMAC, $salt);
 		hash_update($context, $data);
